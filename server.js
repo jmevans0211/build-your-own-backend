35e35174f236a02bca3b164af45e63a881985e4b
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-24 15:00:20 -0700

    Add articulation for lines 1 through 66

--- server.js
+++ server.js
@@ -1,46 +1,66 @@
 const express = require('express');
+//gets access the express framework
 const app = express();
+//makes 'app' an express app
 const environment = process.env.NODE_ENV || 'development';
+//specifies environment that application would be running in
 const configuration = require('./knexfile')[environment];
+//requiring environment from knexfile, hence the bracket notation specifing `environment` from above
 const database = require('knex')(configuration);
+//connecting to databse through knex using the enviormnet settings that have been specified above
 
 
 app.set('port', process.env.PORT || 3000);
+//accessing our local port, 3000
 app.use(express.json());
 
+
 app.get('/api/v1/continents', (request, response) => {
+  //handling a get request for all of the continents. the callback function is our second argument taking two argumnets of resposne and request
   database('continents').select()
+  //accessing the correct database, continents.
     .then((continents) => {
       response.status(200).json(continents)
     })
+    //waiting for the continents data and if the get request has been made with success, make the status code 200 and json all continents data
     .catch((error) => {
-      response.status(500).json({ error });
+      response.status(500).json({ error: 'Internal server error, please try again later' });
+      //if the request is not successful, respond with the status code will be a 500 internal server error
     });
 });
 
 app.get('/api/v1/countries', (request, response) => {
+  //handles get request for all countries.
   database('countries').select()
+  //accessing the correct database, countries.
     .then((countries) => {
       response.status(200).json(countries)
     })
+     //waiting for the countries data and if the get request has been made with success, make the status code 200 and json all countries data
     .catch((error) => {
-      response.status(500).json({ error });
+      response.status(500).json({ error: 'Internal server error, please try again later.' });
+      //run the catch when the request has not been successful and respond with an internal server error (status code 500)
     });
 });
 
 app.get('/api/v1/countries/:id', (request, response) => {
+  //handles get request for a single country
   database('countries').where('id', request.params.id).select()
+  //getting access to the countries data base, and then with the where statement, selecting that id corresponding to the id inputted into the url
     .then(country => {
       if (country.length) {
         response.status(200).json(country);
+        //if there is a row (with a length, same as, length > 0) of data that corresponds to that id, json that country data as a response
       } else {
         response.status(404).json({ 
           error: `Could not find country with an id of ${request.params.id}`
+          //if there is no row with that information, a 404 error will be returned
         });
       }
     })
     .catch(error => {
-      response.status(500).json({ error });
+      response.status(500).json({ error: 'Internal server error. Please try again later.' });
+      //a catch if the server is down 
     });
 });
 
